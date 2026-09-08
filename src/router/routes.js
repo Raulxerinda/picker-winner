6ff3a1d0f0843f74c6a5d0a573847b7e0416bad2
Raul Xerinda <raulxerinda74@gmail.com>
2026-09-08 14:46:55 +0200

    Adicionado a  página Winner Picker

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,7 @@ const routes = [
     path: '/',
     component: () => import('@/layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('@/pages/IndexPage.vue') },
+      { path: '', component: () => import('@/pages/WinnerPicker.vue') },
       { path: 'second', component: () => import('@/pages/SecondPage.vue') },
     ],
   },
